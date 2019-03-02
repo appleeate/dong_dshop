9bf2a539d285763d953ffac527134346f37df194
appleeate <844243709@qq.com>
2019-03-02 13:55:03 +0800

    init dshop

---
+++ src/api/ajax.js
@@ -0,0 +1,43 @@
+/*
+* ajax请求函数模块
+* 返回值:promise对象(异步返回的数据是: response.data)*/
+export default function (url,data={},type='Get') {
+
+  return new Promise(function (resolve,reject) {
+    //执行异步ajax请求
+    let promise
+    if (type === 'GET') {
+      //准备url query参数数据
+      let dataStr = ''//数据拼接字符串
+      object.keys(data).forEach(key => {
+        dataStr += key + '=' + data[key] + '&'
+      })
+      if (dataStr !== '') {
+        dataStr = dataStr.substring(0, dataStr.lastIndexOf('&'))
+        url = url + '?' + dataStr
+      }
+      //发送get请求
+      promise = axios.get(url)
+    } else {
+      //发送post请求
+      promise = axios.post(url,data)
+    }
+    promise.then(function (response) {
+    //成功了调用resolve()
+      resolve(response.data)
+    }).catch(function (error) {
+      reject(error)
+    })
+    // 失败了调用reject()
+  })
+
+
+  return promise
+}
+
+/*
+const response = await ajax()
+const result = response.data
+
+const resule = await ajax()
+*/
